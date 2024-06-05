ac9f61368606dd2f67c6c2ad9f6875f521b66526
AbduRahman <abdrahaman.dev@gmail.com>
2024-06-05 16:16:57 +0000

    pass 3nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,8 +15,24 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                println!("accepted new connection");
-                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                let mut buffer = [0; 1024];
+                _stream.read(&mut buffer).unwrap();
+
+                let header = std::str::from_utf8(&buffer).unwrap();
+                let request_target = header
+                    .lines()
+                    .next()
+                    .unwrap_or("")
+                    .split_whitespace()
+                    .nth(1)
+                    .unwrap_or("");
+                if request_target != '/'.to_string() {
+                    let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+                    _stream.write(response.as_bytes()).unwrap();
+                } else {
+                    let response = "HTTP/1.1 200 OK\r\n\r\nHello, world!";
+                    _stream.write(response.as_bytes()).unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
